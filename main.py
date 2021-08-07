d67334b77be2d87f614d7990a50858b4bcdd545f
Paul Theceri <paul.theceri@gmail.com>
2021-08-07 14:49:34 +0300

    made db and form and ensured that the data gets to db from form

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 from flask import Flask, app, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
 from configs.base import DevelopmentConfig
+
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
-@app.route('/')
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+db = SQLAlchemy(app)
+
+from models.User import User
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+
     return render_template('index.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+
+        new_user = User(first_name = first_name, last_name = last_name, email = email)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+        
+    return render_template('register.html')
---
+++ models/User.py
@@ -0,0 +1,9 @@
+from main import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
